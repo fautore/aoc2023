b93540afed859ee260edb453fe2dbd95a6025d6d
fautore <michele.contato@protonmail.com>
2024-02-09 01:40:14 +0100

    finished day2/part1:

--- day2/src/main.zig
+++ day2/src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-fn readFile(allocator: std.mem.Allocator, filename: []const u8) !std.ArrayList([]u8) {
+fn readFile(allocator: std.mem.Allocator, filename: []const u8) !std.ArrayList(u8) {
     const file = try std.fs.cwd().openFile(filename, .{});
     defer file.close();
 
@@ -8,14 +8,9 @@ fn readFile(allocator: std.mem.Allocator, filename: []const u8) !std.ArrayList([
     const buff = try file.readToEndAlloc(allocator, stat.size);
     defer allocator.free(buff);
 
-    var lineIterator = std.mem.split(u8, buff, "\n");
-    var lines = std.ArrayList([]u8).init(allocator);
-
-    while (lineIterator.next()) |line| {
-        const newLine = try allocator.dupe(u8, line);
-        try lines.append(newLine);
-    }
-    return lines;
+    var fileContents = std.ArrayList(u8).init(allocator);
+    try fileContents.appendSlice(buff);
+    return fileContents;
 }
 
 const Draw = struct {
@@ -24,69 +19,101 @@ const Draw = struct {
     blue: ?u16,
 };
 
-fn parseDraw(drawStr: []u8) Draw {
-    const drawStrIter = std.mem.splitSequence(u8, drawStr, ", ");
-    var draw = Draw{};
+fn parseDraw(drawStr: []const u8) Draw {
+    var drawStrIter = std.mem.splitSequence(u8, drawStr, ", ");
+    var draw = Draw{ .red = null, .green = null, .blue = null };
     while (drawStrIter.next()) |dice| {
-        const diceIter = std.mem.splitSequence(u8, dice, " ");
-        const value = diceIter.next() catch |err| {
-            _ = err;
-        };
-        const parsedValue = std.fmt.parseInt(u16, value, 10) catch |err| {
-            std.debug.print("err", .{err});
-        };
-        const color = diceIter.next() catch |err| {
-            _ = err;
-        };
-        if (std.mem.eql(u8, color, "red")) {
-            draw.red = parsedValue;
-        } else if (std.mem.eql(u8, color, "green")) {
-            draw.green = parsedValue;
-        } else if (std.mem.eql(u8, color, "blue")) {
-            draw.blue = parsedValue;
+        var diceIter = std.mem.splitSequence(u8, dice, " ");
+        const value = if (diceIter.next()) |val| val else "0";
+        if (std.fmt.parseInt(u16, value, 10)) |val| {
+            if (diceIter.next()) |color| {
+                if (std.mem.eql(u8, color, "red")) {
+                    draw.red = val;
+                } else if (std.mem.eql(u8, color, "green")) {
+                    draw.green = val;
+                } else if (std.mem.eql(u8, color, "blue")) {
+                    draw.blue = val;
+                }
+            }
+        } else |err| {
+            std.debug.print("{}", .{err});
         }
     }
     return draw;
 }
 
+pub fn isDrawIllegal(self: Draw, redLimit: u16, greenLimit: u16, blueLimit: u16) bool {
+    var illegal = false;
+    if (self.red) |red| {
+        illegal = illegal or red > redLimit;
+    }
+    if (self.green) |green| {
+        illegal = illegal or green > greenLimit;
+    }
+    if (self.blue) |blue| {
+        illegal = illegal or blue > blueLimit;
+    }
+    return illegal;
+}
+
 const Game = struct {
     Id: u32,
-    first_draw: Draw,
-    second_draw: Draw,
-    third_draw: Draw,
+    draws: std.ArrayList(Draw),
 };
 
+fn isGameImpossible(self: Game, redLimit: u16, greenLimit: u16, blueLimit: u16) bool {
+    var impossible = false;
+    for (self.draws.items) |draw| {
+        std.debug.print("id: {} => r{?} g{?} b{?}", .{ self.Id, draw.red, draw.green, draw.blue });
+        impossible = impossible or isDrawIllegal(draw, redLimit, greenLimit, blueLimit);
+    }
+    if (impossible) {
+        std.debug.print("{} is impossible\n\n", .{self.Id});
+    }
+    return impossible;
+}
+
 fn solvePart1(input: std.ArrayList(u8)) u32 {
-    const solution: u32 = 0;
+    var solution: u32 = 0;
 
     var linesIter = std.mem.splitScalar(u8, input.items, '\n');
     var games = std.ArrayList(Game).init(std.heap.page_allocator);
     while (linesIter.next()) |row| {
-        std.debug.print("\n{s}", .{row});
+        //std.debug.print("\n{s}", .{row});
         var lineIter = std.mem.splitSequence(u8, row, ": ");
         const gameStr = lineIter.first();
-        var gameTokenizer = std.mem.tokenizeSequence(u8, gameStr, "Game ");
-        if (gameTokenizer.peek()) |gameIdStr| {
-            std.debug.print("gameId: {s}", .{gameIdStr});
-            if (std.fmt.parseInt(u32, gameIdStr, 10)) |gameId| {
-                if (lineIter.peek()) |gameCubes| {
-                    var gameStrIter = std.mem.splitSequence(u8, gameCubes, "; ");
-                    const first_draw: Draw = parseDraw(gameStrIter.next()) catch |err| {
-                        std.debug.print("{}", .{err});
-                    };
-                    const second_draw: Draw = parseDraw(gameStrIter.next()) catch |err| {
-                        std.debug.print("{}", .{err});
-                    };
-                    const third_draw: Draw = parseDraw(gameStrIter.next()) catch |err| {
-                        std.debug.print("{}", .{err});
-                    };
-                    games.append(Game{ .Id = gameId, .first_draw = first_draw, .second_draw = second_draw, .third_draw = third_draw }) catch |err| {
-                        std.debug.print("error {}", .{err});
-                    };
+        if (std.mem.indexOf(u8, gameStr, " ")) |spacePos| {
+            const gameIdStr = gameStr[spacePos + 1 ..];
+            const trimmedNumberPart = std.mem.trim(u8, gameIdStr, " \n\r\t");
+            if (std.fmt.parseInt(u32, trimmedNumberPart, 10)) |gameId| {
+                var game = Game{ .Id = gameId, .draws = std.ArrayList(Draw).init(std.heap.page_allocator) };
+                if (lineIter.peek()) |cubeDraws| {
+                    var cubeDrawIterator = std.mem.splitSequence(u8, cubeDraws, "; ");
+                    while (cubeDrawIterator.next()) |drawStr| {
+                        std.debug.print("game {} drawStr {s}\n", .{ gameId, drawStr });
+                        const draw: Draw = parseDraw(drawStr);
+                        game.draws.append(draw) catch |err| {
+                            std.debug.print("error {}", .{err});
+                        };
+                    }
                 }
+                games.append(game) catch |err| {
+                    std.debug.print("{}", .{err});
+                };
             } else |err| {
-                std.debug.print("failed to parse game id {}", .{err});
+                std.debug.print("failed to parse game id {}, idStr {s}\n", .{ err, gameIdStr });
             }
+        }
+    }
+    const redCubeLimit: u16 = 12;
+    const greenCubeLimit: u16 = 13;
+    const blueCubeLimit: u16 = 14;
+    for (games.items) |game| {
+        if (isGameImpossible(game, redCubeLimit, greenCubeLimit, blueCubeLimit)) {
+            std.debug.print("{} is impossible\n", .{game.Id});
+        } else {
+            std.debug.print("{} is possible\n", .{game.Id});
+            solution = solution + game.Id;
         }
     }
     return solution;
@@ -106,17 +133,17 @@ pub fn main() !void {
     const allocator = std.heap.page_allocator;
     var lines = try readFile(allocator, "input/input");
     defer lines.deinit();
-    std.debug.print("part 1 total: {}\n", .{solvePart1(lines.items)});
-    std.debug.print("part 2 total: {}\n", .{solvePart2(lines.items)});
+    std.debug.print("part 1 total: {}\n", .{solvePart1(lines)});
+    //std.debug.print("part 2 total: {}\n", .{solvePart2(lines.items)});
 }
 
 test "test part 1" {
     const input =
-        \\ Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        \\ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-        \\ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        \\ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-        \\ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+        \\Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        \\Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+        \\Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+        \\Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+        \\Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
     ;
     const allocator = std.heap.page_allocator;
     var fileContents = std.ArrayList(u8).init(allocator);
